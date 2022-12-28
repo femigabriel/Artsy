cf6d8bded5f380cdd677c047eb4f832dd40dd2f5
femigab <oluwafemi.gabriel@charisol.io>
2022-12-28 02:16:44 +0100

    auction page css fixes

--- component/auctions/LiveAuctionPage.js
+++ component/auctions/LiveAuctionPage.js
@@ -33,53 +33,55 @@ function LiveAuctionPage() {
   };
   return (
     <div>
-      <div className="flex justify-center item-center ">
-        <div className="border border-[#1E1E1E] flex pr-5">
+      <div className=" ">
+        <div className="">
           <h1 className="text-[#fff] text-[30px] font-semibold relative">
             Current bid $4500
           </h1>
-          <div className="">
-            <img src={live} className="w-[460px] " />
-          </div>
-          <List className="">
-            <VirtualList
-              data={data}
-              height={ContainerHeight}
-              itemHeight={47}
-              itemKey="email"
-              onScroll={onScroll}
-              // width= {1000}
-            >
-              {(item) => (
-                <List.Item key={item.email}>
-                  <List.Item.Meta
-                    avatar={<Avatar src={item.picture.large} />}
-                    title={<a href="https://ant.design">{item.name.last}</a>}
-                    description={item.email}
-                  />
-                </List.Item>
-              )}
-            </VirtualList>
+          <div className="border border-[#1E1E1E] flex">
+            <div className="">
+              <img src={live} className="w-[460px] " />
+            </div>
+            <List className="">
+              <VirtualList
+                data={data}
+                height={ContainerHeight}
+                itemHeight={47}
+                itemKey="email"
+                onScroll={onScroll}
+                // width= {1000}
+              >
+                {(item) => (
+                  <List.Item key={item.email}>
+                    <List.Item.Meta
+                      avatar={<Avatar src={item.picture.large} />}
+                      title={<a href="https://ant.design">{item.name.last}</a>}
+                      description={item.email}
+                    />
+                  </List.Item>
+                )}
+              </VirtualList>
 
-            <div className="p-5 pb-0 pr-10">
-              <div className="flex flex-col">
-                <span className="text-[#BAB9B9] font-italic text-[14px] pb-5">
-                  Creator : Stormi Rylie
-                </span>
-                <div className="flex justify-between w-full">
-                  <div className="live-chat  p-5 w-full ">
-                    <input className="border-0 " />
-                    <button className="send-btn m-3">
-                      <img src={send} className="" />
-                    </button>
-                  </div>
-                  <span className="border rounded-full m-5 mb-5 w-[58px] h-[58px] ">
-                    <img src={likes} className="m-3 ml-2 w-[38px] h-[25px]" />
+              <div className="p-5 pb-0 pr-10">
+                <div className="flex flex-col">
+                  <span className="text-[#BAB9B9] font-italic text-[14px] pb-5">
+                    Creator : Stormi Rylie
                   </span>
+                  <div className="flex justify-between w-full">
+                    <div className="live-chat  p-5 w-full ">
+                      <input className="border-0 " />
+                      <button className="send-btn m-3">
+                        <img src={send} className="" />
+                      </button>
+                    </div>
+                    <span className="border rounded-full m-5 mb-5 w-[58px] h-[58px] ">
+                      <img src={likes} className="m-3 ml-2 w-[38px] h-[25px]" />
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </List>
+            </List>
+          </div>
         </div>
       </div>
       <div className=" p-10 pl-64 flex">
